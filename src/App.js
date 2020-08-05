422124cecb59f932970257514e479c1776b2aadd
florenciafer <fernandez.gestoria@hotmail.com>
2020-08-05 19:42:48 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Results from "./pages/Results";
+import Questions from "./pages/questions";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/home" component={Home}></Route>
+        <Route exact path="/results" component={Results}></Route>
+        <Route exact path="/questions/:id" component={Questions}></Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
